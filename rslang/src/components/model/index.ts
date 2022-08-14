5b905ef4f661b98e80c362e2a40021d0dcfa8cdd
Aibek <ai.bek@mail.ru>
2022-08-14 17:20:56 +0600

    refactor: change methods code

--- rslang/src/components/model/index.ts
+++ rslang/src/components/model/index.ts
@@ -1,34 +1,49 @@
 import { IUser, IWord } from '../../types/index';
 
-const baseURL = "http://localhost:3000"
+const baseURL = 'http://localhost:3000';
 enum Path {
   words = '/words',
+  users = '/users',
 }
 
-export class Model {
-
-  async getWords(page: number, groupNum: number): Promise<IWord[] | undefined> {
+export default class Model {
+  async getWords(page: number, groupNum: number): Promise<IWord[]> {
+    let words: IWord[] = [];
     try {
       const response = await fetch(`${baseURL}${Path.words}?page=${page}&group=${groupNum}`);
-      const words = await <Promise<IWord[]>>response.json();
-      return words;
+      words = await (<Promise<IWord[]>>response.json());
+    } catch (error) {
+      console.error(error);
+    }
+    return words;
+  }
+
+  async createUser(user: IUser): Promise<void> {
+    let status = 0;
+    try {
+      const response = await fetch(`${baseURL}${Path.users}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      status = response.status;
+
+      switch (status) {
+        case 200:
+          console.log('new user created successfully');
+          break;
+        case 417:
+          throw new Error('user with same email already registered');
+        case 422:
+          throw new Error('Incorrect e-mail or password');
+        default:
+          break;
+      }
     } catch (error) {
       console.error(error);
     }
   }
-
-  async createUser(user: IUser): Promise<void> {
-    try {
-      await fetch('https://<your-app-name>.herokuapp.com/users', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
 }
