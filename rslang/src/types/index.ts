ff7897ed70e8643aeca3f70dc5c90eb56e7697d8
Aibek <ai.bek@mail.ru>
2022-08-14 11:20:54 +0600

    feat: add file with types

---
+++ rslang/src/types/index.ts
@@ -0,0 +1,50 @@
+export interface IWord {
+  id: string,
+  group: number,
+  page: number,
+  word: string,
+  image: string,
+  audio: string,
+  audioMeaning: string,
+  audioExample: string,
+  textMeaning: string,
+  textExample: string,
+  transcription: string,
+  wordTranslate: string,
+  textMeaningTranslate: string,
+  textExampleTranslate: string
+}
+
+
+interface IOptional {
+  [key: string]: string | boolean;
+}
+
+export interface IUserWord {
+  difficulty: string,
+  optional?: IOptional
+}
+
+export interface IUser {
+  name: string,
+  email: string,
+  password: string
+}
+
+export interface IAuth {
+  message: string,
+  token: string,
+  refreshToken: string,
+  userId: string,
+  name: string
+}
+
+export interface ISetting {
+  wordsPerDay: number,
+  optional?: IOptional
+}
+
+export interface IStatistic {
+  learnedWords: number,
+  optional?: IOptional
+}
