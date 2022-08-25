61b58905c1741fcdbd2c8c0d3df4ad437e8137e5
diana2886 <tdm2886@gmail.com>
2022-08-25 22:02:13 +0600

    feat: add level button and page button listeners

--- rslang/src/controller/textbookController.ts
+++ rslang/src/controller/textbookController.ts
@@ -1,7 +1,12 @@
-import Model, { baseURL } from '../model/components/index';
+import ApiModel, { baseURL } from '../model/components/index';
+import TextbookModel from '../model/textbookModel';
+import PageIds from '../view/pages/app/pageIds';
+import TextbookPage from '../view/pages/textbook/index';
 
 class TextbookController {
-  listenPlayWordButton(page: number, group: number) {
+  textbookPage = new TextbookPage(PageIds.Textbook);
+
+  listenPlayWordButton() {
     document.body.addEventListener('click', (e) => {
       const target = e.target as HTMLElement;
       const audioPlay = (paths: string[]) => {
@@ -15,8 +20,9 @@ class TextbookController {
         };
       };
       if (target.classList.contains('word__play')) {
+        console.log(target.id);
         (async () => {
-          const words = await Model.getWords(page, group);
+          const words = await ApiModel.getWords(TextbookModel.page, TextbookModel.group);
           words.forEach((item) => {
             const audios = [
               `${baseURL}/${item.audio}`,
@@ -24,10 +30,61 @@ class TextbookController {
               `${baseURL}/${item.audioExample}`,
             ];
             if (item.id === target.id) {
+              // console.log(target.id, item.id);
               audioPlay(audios);
             }
           });
         })().catch((err: Error) => console.warn(err.message));
+      }
+    });
+  }
+
+  rerenderWords() {
+    const wordsWrapper = document.querySelector('.words__wrapper') as HTMLElement;
+    wordsWrapper.innerHTML = '';
+    const textbookPage = new TextbookPage(PageIds.Textbook);
+    (async () => {
+      wordsWrapper.append(await textbookPage.renderWords(TextbookModel.page, TextbookModel.group));
+    })().catch((err: Error) => console.warn(err.message));
+  }
+
+  listenLevelButton() {
+    document.body.addEventListener('click', (e) => {
+      const target = e.target as HTMLElement;
+      if (target.classList.contains('level__item')) {
+        (document.querySelector('.levels-btn') as HTMLButtonElement).innerHTML = target.innerHTML;
+        TextbookModel.group = +target.id.split('level')[1] - 1;
+        TextbookModel.page = 0;
+        (document.querySelector('.pages-btn') as HTMLButtonElement).innerHTML = `Page ${TextbookModel.page + 1}`;
+        this.rerenderWords();
+      }
+    });
+  }
+
+  listenPageButton() {
+    document.body.addEventListener('click', (e) => {
+      const target = e.target as HTMLElement;
+      const pagesButton = document.querySelector('.pages-btn') as HTMLButtonElement;
+      if (target.classList.contains('page__item')) {
+        pagesButton.innerHTML = target.innerHTML;
+        TextbookModel.page = +target.id.split('page')[1] - 1;
+        const wordsWrapper = document.querySelector('.words__wrapper') as HTMLElement;
+        wordsWrapper.innerHTML = '';
+        const textbookPage = new TextbookPage(PageIds.Textbook);
+        (async () => {
+          wordsWrapper.append(await textbookPage.renderWords(TextbookModel.page, TextbookModel.group));
+        })().catch((err: Error) => console.warn(err.message));
+      }
+      if (target.closest('.page-prev')) {
+        if (TextbookModel.page > 0) TextbookModel.page -= 1;
+        pagesButton.innerHTML = `Page ${TextbookModel.page + 1}`;
+        this.rerenderWords();
+      }
+      if (target.closest('.page-next')) {
+        const PAGES_AMOUNT = 30;
+        if (TextbookModel.page < PAGES_AMOUNT) TextbookModel.page += 1;
+        pagesButton.innerHTML = `Page ${TextbookModel.page + 1}`;
+        this.rerenderWords();
       }
     });
   }
