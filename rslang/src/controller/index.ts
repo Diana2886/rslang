a85cc4df52fdbc51f1b19b17fd877779ed0dc056
diana2886 <tdm2886@gmail.com>
2022-08-25 22:15:42 +0600

    feat: add listeners to controller/index.ts

--- rslang/src/controller/index.ts
+++ rslang/src/controller/index.ts
@@ -1,17 +1,19 @@
-import Model from '../model/components/index';
+import ApiModel from '../model/components/index';
 import AppView from '../view/pages/app/index';
 import HeaderController from './headerController';
 import TextbookController from './textbookController';
 
 class App {
   start() {
-    const model = new Model();
+    const model = new ApiModel();
     const view = new AppView();
     view.render();
     const headerController = new HeaderController();
     headerController.listenHeaderButtons();
     const textbookController = new TextbookController();
-    textbookController.listenPlayWordButton(0, 0);
+    textbookController.listenPlayWordButton();
+    textbookController.listenLevelButton();
+    textbookController.listenPageButton();
   }
 }
 
