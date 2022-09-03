f2304d656a67d450ebc1906bb79166614c1ebe7a
diana2886 <tdm2886@gmail.com>
2022-09-03 22:36:16 +0600

    refactor: add some changes

--- rslang/src/controller/textbookController.ts
+++ rslang/src/controller/textbookController.ts
@@ -183,9 +183,8 @@ class TextbookController {
                 if (target.classList.contains(`${item}-button`)) {
                   wordContainer.style.backgroundColor = 'inherit';
                   target.classList.add(item);
-                  if (target.classList.contains('difficult'))
+                  if (target.classList.contains(item))
                     await this.model.updateUserWord(wordId, { difficulty: 'new', optional: userWord.optional });
-                  else if (target.classList.contains('learned')) await this.model.deleteUserWord(wordId);
                   this.textbookModel.resetPageStyles();
                   await this.textbookModel.checkPageStyle();
                 }
